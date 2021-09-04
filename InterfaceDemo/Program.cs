294c8b75ea9524638a80efaabf23d68f526a54ef
taner <taner@DESKTOP-8FBSMG9>
2021-09-04 19:03:06 +0300

    Add project files.

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    interface IWorker
+    {   
+        void work(); 
+    }
+    interface IEat
+    {
+        void eat();
+    }
+    interface IGetSalary
+    {
+        void getSalary();
+    }
+
+    class Manager : IWorker, IGetSalary, IEat
+    {
+        public void eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Worker : IWorker,IEat, IGetSalary
+    {
+        public void eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Robot : IWorker
+    {
+        public void work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
